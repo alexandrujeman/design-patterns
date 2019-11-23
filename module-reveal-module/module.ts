62e40e29e1bf0c6f8f780b4eeaeeed348a90e330
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-11-23 11:34:42 +0200

    Add module & revealing module pattern

---
+++ module-reveal-module/module.ts
@@ -0,0 +1,42 @@
+// Standard module pattern
+const UIControl = (function() {
+  let text = "Hello world";
+
+  const changeText = function() {
+    const element = document.querySelector("h1");
+    element.textContent = text;
+  };
+
+  return {
+    callChangeText: function() {
+      changeText();
+    }
+  };
+})();
+
+UIControl.callChangeText();
+
+// Revealing module patter
+const ItemControl = (function() {
+  let data = [];
+
+  function add(item) {
+    data.push(item);
+    console.log("Item added...");
+  }
+
+  function get(id) {
+    return data.find(item => {
+      return item.id === id;
+    });
+  }
+
+  return {
+    add: add,
+    get: get
+  };
+})();
+
+console.log('Reveal module pattern')
+ItemControl.add({ id: 1, name: "John" });
+console.log(ItemControl.get(1));
